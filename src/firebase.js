7239cbae24a275d74fe12f86431997587e64d6da
jatin verma <jv.jatinverma.jv@gmail.com>
2023-09-15 19:56:03 +0530

    fixing gitignore

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDemsmwLP_FL_JDxyJ13PfW_8w6rSb9zXM",
+  authDomain: "mini-twitter-clone-84f68.firebaseapp.com",
+  projectId: "mini-twitter-clone-84f68",
+  storageBucket: "mini-twitter-clone-84f68.appspot.com",
+  messagingSenderId: "577772723530",
+  appId: "1:577772723530:web:e56bc4eb820786b529b5b3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
